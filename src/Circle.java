5315bab90e96d99c981e27b31f3fcdba51584fe1
bolotbekovaaidana <aidanabolotbekova42gmail.com>
2022-12-01 17:38:41 +0600

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,14 @@
+public class Circle {
+    private static double pi = Math.PI;
+
+    public static double area(double radius) {
+        return pi * (radius * radius);
+
+    }
+
+    public static double circumference(double radius) {
+return pi * 2 * radius;
+    }
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double r=4;
+        Circle circle=new Circle();
+        System.out.println("Area: "+Circle.area(r));
+        System.out.println("Circumference: "+Circle.circumference(r));
+    }
+}
